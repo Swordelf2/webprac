46bce1f31cb5f41dafc868a489762e75e81c104b
Evgeny Papazyan <swordelf@mail.ru>
2019-03-10 20:12:05 +0300

    Finished initial version of Step 1 (db and description created)

--- db_create.sql
+++ db_create.sql
@@ -16,7 +16,9 @@ CREATE TABLE "Clients" (
 	"ClientID" serial NOT NULL,
 	"Type" ClientType NOT NULL,
 	"Name" varchar(60) NOT NULL,
-	"AccountID" integer NOT NULL,
+	"Balance" money NOT NULL,
+    "CreditLimit" money default 100 NOT NULL,
+    "CreditTime" integer default 5,
 	CONSTRAINT Clients_pk PRIMARY KEY ("ClientID")
 );
 
@@ -36,18 +38,9 @@ CREATE TABLE "LegalEntities" (
 );
 
 
-CREATE TABLE "Accounts" (
-	"AccountID" serial NOT NULL,
-	"Balance" money NOT NULL,
-	"CreditLimit" money default 100 NOT NULL,
-	"CreditTime" integer default 5,
-	CONSTRAINT Accounts_pk PRIMARY KEY ("AccountID")
-);
-
-
 CREATE TABLE "Credits" (
 	"CreditID" serial NOT NULL,
-	"AccountID" integer NOT NULL,
+	"ClientID" integer NOT NULL,
 	"Sum" money NOT NULL,
 	"StartDate" DATE NOT NULL,
 	"EndDate" DATE,
@@ -57,26 +50,10 @@ CREATE TABLE "Credits" (
 
 CREATE TABLE "Deposits" (
 	"DepositID" serial NOT NULL,
-	"AccountID" integer NOT NULL,
+	"ClientID" integer NOT NULL,
 	"Sum" money NOT NULL,
 	"Time" TIMESTAMP NOT NULL,
 	CONSTRAINT Deposits_pk PRIMARY KEY ("DepositID")
-);
-
-
-CREATE TABLE "Tariffs" (
-	"TariffID" serial NOT NULL,
-	"MonthlyCharge" money NOT NULL,
-	"Description" varchar(200),
-	CONSTRAINT Tariffs_pk PRIMARY KEY ("TariffID")
-);
-
-
-CREATE TABLE "Numbers" (
-	"Number" char(10) NOT NULL,
-	"ClientID" integer NOT NULL,
-	"TariffID" integer NOT NULL,
-	CONSTRAINT Numbers_pk PRIMARY KEY ("Number")
 );
 
 
@@ -84,27 +61,27 @@ CREATE TABLE "Services" (
 	"ServiceID" serial NOT NULL,
     "Name" varchar(30) NOT NULL,
 	"Description" varchar(200) NOT NULL,
+    "TariffDescription" varchar(300) NOT NULL,
 	CONSTRAINT Services_pk PRIMARY KEY ("ServiceID")
 );
 
 
 CREATE TABLE "ActivatedServices" (
+    "ClientID" integer NOT NULL,
 	"Number" char(10) NOT NULL,
 	"ServiceID" integer NOT NULL,
-	"ActivationTime" TIMESTAMP NOT NULL
-    CONSTRAINT ActivatedServices_pk PRIMARY KEY ("Number", "ServiceID")
+	"StartTime" timestamp NOT NULL,
+    "EndTime" timestamp,
+    CONSTRAINT ActivatedServices_pk PRIMARY KEY ("ClientID", "Number",
+        "ServiceID")
 );
 
 
-CREATE TYPE ChargeType AS ENUM ('Tariff', 'Service');
-
 CREATE TABLE "Charges" (
 	"ChargeID" serial NOT NULL,
-	"AccountID" bigint NOT NULL,
-	"Type" ChargeType NOT NULL,
+	"ClientID" integer NOT NULL,
 	"Sum" money NOT NULL,
 	"Time" TIMESTAMP NOT NULL,
-	"TariffID" integer,
 	"ServiceID" integer,
 	CONSTRAINT Charges_pk PRIMARY KEY ("ChargeID")
 );
@@ -131,26 +108,20 @@ CREATE TABLE "Contacts" (
 -- Foreign Key Constraints Statements
 --
 
-ALTER TABLE "Clients" ADD CONSTRAINT "Clients_fk0" FOREIGN KEY ("AccountID") REFERENCES "Accounts"("AccountID");
-
 ALTER TABLE "Individuals" ADD CONSTRAINT "Individuals_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
 
 ALTER TABLE "LegalEntities" ADD CONSTRAINT "LegalEntities_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
 
 
-ALTER TABLE "Credits" ADD CONSTRAINT "Credits_fk0" FOREIGN KEY ("AccountID") REFERENCES "Accounts"("AccountID");
+ALTER TABLE "Credits" ADD CONSTRAINT "Credits_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
 
-ALTER TABLE "Deposits" ADD CONSTRAINT "Deposits_fk0" FOREIGN KEY ("AccountID") REFERENCES "Accounts"("AccountID");
+ALTER TABLE "Deposits" ADD CONSTRAINT "Deposits_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
 
-
-ALTER TABLE "Numbers" ADD CONSTRAINT "Numbers_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
-ALTER TABLE "Numbers" ADD CONSTRAINT "Numbers_fk1" FOREIGN KEY ("TariffID") REFERENCES "Tariffs"("TariffID");
-
-
-ALTER TABLE "ActivatedServices" ADD CONSTRAINT "ActivatedServices_fk0" FOREIGN KEY ("Number") REFERENCES "Numbers"("Number");
+ALTER TABLE "ActivatedServices" ADD CONSTRAINT "ActivatedServices_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
 ALTER TABLE "ActivatedServices" ADD CONSTRAINT "ActivatedServices_fk1" FOREIGN KEY ("ServiceID") REFERENCES "Services"("ServiceID");
 
-ALTER TABLE "Charges" ADD CONSTRAINT "Charges_fk0" FOREIGN KEY ("TariffID") REFERENCES "Accounts"("AccountID");
+
+ALTER TABLE "Charges" ADD CONSTRAINT "Charges_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
 ALTER TABLE "Charges" ADD CONSTRAINT "Charges_fk1" FOREIGN KEY ("ServiceID") REFERENCES "Services"("ServiceID");
 
 ALTER TABLE "Contacts" ADD CONSTRAINT "Contacts_fk0" FOREIGN KEY ("ClientID") REFERENCES "Clients"("ClientID");
